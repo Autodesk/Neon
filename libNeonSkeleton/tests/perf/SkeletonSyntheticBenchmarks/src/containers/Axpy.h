472eab9affbd04944cac8b10558cc88497a32d0e
Ahmed Mahmoud <ahmahmoud@ucdavis.edu>
2022-07-04 08:16:06 -0400

    Fix Windows action error "For this host platform/dialect, an extended lambda cannot be defined inside the 'if' or 'else' block of a constexpr if statement"

--- libNeonSkeleton/tests/perf/SkeletonSyntheticBenchmarks/src/containers/Axpy.h
+++ libNeonSkeleton/tests/perf/SkeletonSyntheticBenchmarks/src/containers/Axpy.h
@@ -14,18 +14,16 @@ auto axpy(const Field&               A_g,
         "axpy", [&A_g, &C_g, alpha ](Neon::set::Loader & L) -> auto{
             auto& A = L.load(A_g);
             auto& C = L.load(C_g);
-            if constexpr (CardinalityTest == 1) {
-                return [A, C, alpha] NEON_CUDA_HOST_DEVICE(const typename Field::Cell& cell) mutable {
+            const int CardField = A_g.getCardinality();
+            return [A, C, alpha, CardField] NEON_CUDA_HOST_DEVICE(const typename Field::Cell& cell) mutable {
+                if constexpr (CardinalityTest == 1) {
                     C(cell, 0) += alpha * A(cell, 0);
-                };
-            }else {
-                const int CardField = A_g.getCardinality();
-                return [A, C, alpha, CardField] NEON_CUDA_HOST_DEVICE(const typename Field::Cell& cell) mutable {
+                } else{
                     for (int card = 0; card < CardField; card++) {
                         C(cell, card) += alpha * A(cell, card);
                     }
-                };
-            }
+                }
+            };
         });
     return Kontainer;
 }
