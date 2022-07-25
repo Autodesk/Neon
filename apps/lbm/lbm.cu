dbb927ab52edd67e0f8e77ad7879937612fe2a63
Ahmed Mahmoud <ahmahmoud@ucdavis.edu>
2022-07-25 17:35:27 -0400

    containers structure

--- apps/lbm/lbm.cu
+++ apps/lbm/lbm.cu
@@ -1,8 +1,7 @@
 // References
 // 2D LBM: https://github.com/hietwll/LBM_Taichi
-// 2D LBM Verification data:
-// https://www.sciencedirect.com/science/article/pii/0021999182900584 For 2D/3D
-// constants: https://en.wikipedia.org/wiki/Lattice_Boltzmann_methods
+// 2D LBM Verification data: https://www.sciencedirect.com/science/article/pii/0021999182900584
+//For 2D/3D constants: https://en.wikipedia.org/wiki/Lattice_Boltzmann_methods
 
 #include <iomanip>
 #include <sstream>
@@ -36,20 +35,130 @@ Neon::domain::Stencil create_stencil<2, 9>()
 template <>
 Neon::domain::Stencil create_stencil<3, 19>()
 {
-    return Neon::domain::Stencil::s19_t(false);  // filterCenterOut = false;
+    // filterCenterOut = false;
+    return Neon::domain::Stencil::s19_t(false);
 }
 
-template <unsigned int DIM, typename T, typename Grid, typename Field>
-inline void exportVTI(const int t, Field& field, Grid& grid)
+template <typename Field>
+inline void exportVTI(const int t, Field& field)
 {
     printf("\n Exporting Frame =%d", t);
     int                precision = 4;
     std::ostringstream oss;
     oss << std::setw(precision) << std::setfill('0') << t;
-    std::string prefix = "lbm" + std::to_string(DIM) + "D_";
+    std::string prefix = "lbm" + std::to_string(field.getCardinality()) + "D_";
     std::string fname = prefix + oss.str();
     field.ioToVtk(fname, "field");
 }
+
+
+/**
+ * Update the velocity and density of the fluid after the
+ * collide and stream step.
+ * @param in_voxels The input lattice to use for computing
+ *        the next collide and stream step from the 
+ *        previous result.
+ * @param old_voxels The previous output lattice from the last
+ *        computation step to be updated.
+ * @param mask The mask to use for identifying which
+ *        boundary condition to apply to a grid cell.
+ *        0 - Don't apply the collide and stream step.
+ *        1 - Apply the collide and stream step.
+ * @param out_velocity The ouotput velocity.
+ * @param density The computed density from the voxels,
+ *        boundary conditions, and velocity
+ * @param density_temp The density from the voxels,
+ *        used as a temporary field for reading
+ *        as a stencil in the boundary conditions step.
+ * @param tau The characteristic timescale.
+ */
+template <unsigned int DIM,
+          unsigned int COMP,
+          typename RealFieldT,
+          typename MaskFeildT>
+Neon::set::Container computeVelocity(const RealFieldT&         in_voxels,
+                                     RealFieldT&               old_voxels,
+                                     const MaskFeildT&         mask,
+                                     RealFieldT&               out_velocity,
+                                     RealFieldT&               density,
+                                     RealFieldT&               density_temp,
+                                     typename RealFieldT::Type tau)
+{
+    return Neon::set::Container();
+}
+
+
+/**
+ * Apply the collision and streaming step of the LBM algorithm.
+ * This should be the first container in the sequence.
+ * @param density The density of the fluid at each voxel.
+ * @param in_velocity The velocity of the fluid from the 
+ *        previous step.
+ * @param in_voxels The input lattice to use for computing
+ *        the next collide and stream step from the 
+ *        previous result.
+ * @param mask The mask to use for identifying which
+ *        boundary condition to apply to a grid cell.
+ *        0 - Don't apply the collide and stream step.
+ *        1 - Apply the collide and stream step.
+ * @param out_voxels The output lattice where the result will be
+ *         stored.
+ * @param tau The characteristic timescale.
+ */
+template <unsigned int DIM,
+          unsigned int COMP,
+          typename RealFieldT,
+          typename MaskFeildT>
+Neon::set::Container collideAndStream(const RealFieldT&         density,
+                                      const RealFieldT&         in_velocity,
+                                      const RealFieldT&         in_voxels,
+                                      const MaskFeildT&         mask,
+                                      RealFieldT&               out_voxels,
+                                      typename RealFieldT::Type tau)
+{
+    return Neon::set::Container();
+}
+
+/**
+ * Apply boundary conditions to the lattice. This should
+ * be the last container.
+ * @param in_voxels The input lattice to use for computing
+ *        the boundary conditions. This should be the output
+ *        voxels from the previous coontainer.
+ * @param in_velocity The output velocity from the previous
+ *        step.
+ * @param boundary_mask The mask to use for identifying which
+ *        boundary condition to apply to a grid cell.
+ *        0 - no boundary conditions
+ *        1 - Apply fixed boundary conditions
+ *        2 - Apply Neumann boundary conditions
+ * @param read_density The temporary density field from the
+ *        previous container's output. Used to read from as a
+ *        stencil.
+ * @param out_voxels The output lattice where the result will be
+ *         stored.
+ * @param out_velocity The ouotput velocity.
+ * @param density The computed density from the voxels,
+ *        boundary conditions, and velocity
+ * @param tau The characteristic timescale.
+ * @param dim The dimensions of the field.
+ */
+template <unsigned int DIM,
+          unsigned int COMP,
+          typename RealFieldT,
+          typename MaskFeildT>
+Neon::set::Container boundaryConditions(const RealFieldT&         in_voxels,
+                                        const RealFieldT&         in_velocity,
+                                        const MaskFeildT&         boundary_mask,
+                                        const RealFieldT&         read_density,
+                                        RealFieldT&               out_voxels,
+                                        RealFieldT&               out_velocity,
+                                        RealFieldT&               density,
+                                        typename RealFieldT::Type tau)
+{
+    return Neon::set::Container();
+}
+
 
 template <unsigned int DIM, unsigned int COMP, typename RealFieldT, typename MaskFeildT>
 inline void setup(const FlowType flow_type,
@@ -197,6 +306,57 @@ inline void setup(const FlowType flow_type,
 }
 
 
+template <unsigned int DIM, unsigned int COMP, typename RealFieldT, typename MaskFeildT>
+inline void run(const int                       num_frames,
+                const FlowType                  flow_type,
+                MaskFeildT&                     boundary_mask,
+                MaskFeildT&                     center_mask,
+                RealFieldT&                     lattice_1,
+                RealFieldT&                     lattice_2,
+                RealFieldT&                     velocity_1,
+                RealFieldT&                     velocity_2,
+                RealFieldT&                     rho_1,
+                RealFieldT&                     rho_2,
+                const typename RealFieldT::Type tau)
+{
+    const auto& backend = boundary_mask.getBackend();
+
+    Neon::skeleton::Skeleton sk(backend);
+    Neon::skeleton::Options  opt;
+
+    std::vector<Neon::set::Container> containers;
+
+
+    containers.push_back(collideAndStream<DIM, COMP>(
+        rho_1, velocity_1, lattice_1, boundary_mask, lattice_2, tau));
+
+    containers.push_back(computeVelocity<DIM, COMP>(
+        lattice_2, lattice_1, boundary_mask, velocity_2, rho_1, rho_2, tau));
+
+    containers.push_back(boundaryConditions<DIM, COMP>(
+        lattice_2, velocity_2, center_mask, rho_2, lattice_1, velocity_1, rho_1, tau));
+
+
+    sk.sequence(containers, "LBM", opt);
+
+
+    int save_id = 0;
+
+    int t = (DIM == 2) ? 1000 : 40;
+    for (int f = 0; f < num_frames; ++f) {
+        sk.run();
+
+        if (f % t == 0) {
+            backend.syncAll();
+            velocity_1.updateIO(0);
+            exportVTI(save_id, velocity_1);
+            printf("\n frame  %d exported", f);
+            save_id++;
+        }
+    }
+    backend.syncAll();
+}
+
 int main(int argc, char** argv)
 {
     Neon::init();
@@ -205,7 +365,6 @@ int main(int argc, char** argv)
         auto             runtime = Neon::Runtime::stream;
         std::vector<int> gpu_ids{0};
         Neon::Backend    backend(gpu_ids, runtime);
-               
 
         //2D
         //constexpr int DIM = 2;
@@ -214,6 +373,7 @@ int main(int argc, char** argv)
         //3D
         constexpr int DIM = 3;
         constexpr int COMP = 19;
+
 
         const FlowType flow_type = FlowType::border;
 
@@ -252,5 +412,7 @@ int main(int argc, char** argv)
         auto center_mask = grid.template newField<int>("center_mask", rho_cardinality, int(inactive));
 
         setup<DIM, COMP>(flow_type, boundary_mask, center_mask, lattice_1, lattice_2, velocity_1, velocity_2, rho_1, rho_2);
+
+        run<DIM, COMP>(num_frames, flow_type, boundary_mask, center_mask, lattice_1, lattice_2, velocity_1, velocity_2, rho_1, rho_2, tau);
     }
 }
