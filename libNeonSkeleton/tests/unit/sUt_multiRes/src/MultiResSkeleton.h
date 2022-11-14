4484fc6a9ba3eb97cdb9253a8cb4f0afa6161f92
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2022-11-14 11:16:28 -0500

    multi-res on skeleton
    unit test is disabled for now

--- libNeonSkeleton/tests/unit/sUt_multiRes/src/MultiResSkeleton.h
+++ libNeonSkeleton/tests/unit/sUt_multiRes/src/MultiResSkeleton.h
@@ -5,12 +5,13 @@ void MultiResSkeleton()
 {
     using Type = int32_t;
     const int              nGPUs = 1;
-    const Neon::int32_3d   dim(24, 24, 24);
+    const Neon::int32_3d   dim(64, 64, 64);
     const std::vector<int> gpusIds(nGPUs, 0);
 
-    const Neon::domain::internal::bGrid::bGridDescriptor descriptor({1, 1, 1});
+    const Neon::domain::internal::bGrid::bGridDescriptor descriptor({1, 1, 1, 1, 1});
 
-    for (auto runtime : {Neon::Runtime::stream, Neon::Runtime::openmp}) {
+
+    for (auto runtime : {Neon::Runtime::openmp, Neon::Runtime::stream}) {
 
         auto bk = Neon::Backend(gpusIds, runtime);
 
@@ -30,7 +31,7 @@ void MultiResSkeleton()
             descriptor);
         //BGrid.topologyToVTK("bGrid111.vtk", false);
 
-        auto field = grid.newField<Type>("field_in", 3, -1);
+        auto field = grid.newField<Type>("field", 3, -1);
 
 
         //Init fields
@@ -62,29 +63,36 @@ void MultiResSkeleton()
                         local(cell, 0) = global.v[0];
                         local(cell, 1) = global.v[1];
                         local(cell, 2) = global.v[2];
+                        printf("\n cell.BlockId= %d, cell.id= %d, %d, %d, val= %d, %d, %d",
+                               int(cell.mBlockID),
+                               int(cell.mLocation.x),
+                               int(cell.mLocation.y),
+                               int(cell.mLocation.z),
+                               local(cell, 0),
+                               local(cell, 1),
+                               local(cell, 2));
                     };
                 }));
         }
 
-        {
-            //all other levels
-            for (int level = descriptor.getDepth() - 2; level >= 0; --level) {
-                field.setCurrentLevel(level);
-                grid.setCurrentLevel(level);
-                containers.push_back(grid.getContainer(
-                    "kernel" + std::to_string(level),
-                    [&](Neon::set::Loader& loader) {
-                        auto& local = loader.load(field);
-                        return [=] NEON_CUDA_HOST_DEVICE(const typename Neon::domain::bGrid::Cell& cell) mutable {
-                            assert(local.hasParent());
+        //all other levels
+        for (int level = descriptor.getDepth() - 2; level >= 0; --level) {
+            grid.setCurrentLevel(level);
+            field.setCurrentLevel(level);
+            containers.push_back(grid.getContainer(
+                "ReadParent" + std::to_string(level),
+                [&](Neon::set::Loader& loader) {
+                    auto& local = loader.load(field);
+                    return [=] NEON_CUDA_HOST_DEVICE(const typename Neon::domain::bGrid::Cell& cell) mutable {
+                        assert(local.hasParent(cell));
 
-                            local(cell, 0) = local.parent(cell, 0);
-                            local(cell, 1) = local.parent(cell, 1);
-                            local(cell, 2) = local.parent(cell, 2);
-                        };
-                    }));
-            }
+                        local(cell, 0) = local.parent(cell, 0);
+                        local(cell, 1) = local.parent(cell, 1);
+                        local(cell, 2) = local.parent(cell, 2);
+                    };
+                }));
         }
+
 
         Neon::skeleton::Skeleton skl(grid.getBackend());
         skl.sequence(containers, "MultiResSkeleton");
@@ -93,26 +101,31 @@ void MultiResSkeleton()
         grid.getBackend().syncAll();
         if (bk.runtime() == Neon::Runtime::stream) {
             field.updateIO();
+            grid.getBackend().syncAll();
         }
 
-
         //verify
-        for (int l = 0; l < descriptor.getDepth(); ++l) {
+        for (int l = descriptor.getDepth() - 1; l >= 0; --l) {
+            grid.setCurrentLevel(l);
+            field.setCurrentLevel(l);
+
+            int parent_level = descriptor.getDepth() - 1;
             field.forEachActiveCell(
                 l,
-                [&](const Neon::int32_3d, const int, Type& val) {
-                    if (l != descriptor.getDepth() - 1) {
-                        EXPECT_EQ(val, l + 1);
+                [&](const Neon::int32_3d id, const int card, Type& val) {
+                    if (l == descriptor.getDepth() - 1) {
+                        EXPECT_EQ(val, id.v[card]);
                     } else {
-                        EXPECT_EQ(val, l);
+                        Neon::index_3d parent = descriptor.toBaseIndexSpace(descriptor.childToParent(id, parent_level - 1), parent_level);
+                        EXPECT_EQ(val, parent.v[card]) << "Level= " << l;
                     }
                 });
         }
     }
 }
-TEST(MultiRes, Skeleton)
+TEST(MultiRes, DISABLED_Skeleton)
 {
     if (Neon::sys::globalSpace::gpuSysObjStorage.numDevs() > 0) {
-        MultiResParent();
+        MultiResSkeleton();
     }
 }
