cdbc7e0b30ca16a4be47e0217de546fe4cddc067
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-03-23 10:04:43 -0400

    organize plotting code

--- apps/lbmMultiRes/plot.py
+++ apps/lbmMultiRes/plot.py
@@ -2,23 +2,23 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-#only use the colums for y and Re=1000
+def plotme(dat_filename, label, png_filename):
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 3), dpi=200)
 
-fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 3), dpi=200)
+    y_neon, u_neon = np.loadtxt(dat_filename, unpack=True, usecols=(0, 1))
+    p = y_neon.argsort()
+    u_neon = u_neon[p]
+    y_neon = y_neon[p]
+    axes.plot(y_neon, 25*u_neon, 'b-', label=label)
 
+    y_ref, u_ref = np.loadtxt('ghia1982.dat', unpack=True, skiprows=2, usecols=(0, 1))
+    axes.plot(y_ref, u_ref, 'ms', label='Ghia et al. 1982')
 
-y_neon, u_neon = np.loadtxt('NeonUniformLBM_20000.dat', unpack=True, usecols=(0, 1))
-p = y_neon.argsort()
-u_neon = u_neon[p]
-y_neon = y_neon[p]
-axes.plot(y_neon, 25*u_neon, 'b-', label='Neon Uniform LBM')
+    axes.legend()
+    axes.set_xlabel(r'Y')
+    axes.set_ylabel(r'U')
+    plt.tight_layout()
+    plt.savefig(png_filename)
 
-
-y_ref, u_ref = np.loadtxt('ghia1982.dat', unpack=True, skiprows=2, usecols=(0, 1))
-axes.plot(y_ref, u_ref, 'ms', label='Ghia et al. 1982')
-
-axes.legend()
-axes.set_xlabel(r'Y')
-axes.set_ylabel(r'U')
-plt.tight_layout()
-plt.savefig("UniformNeon_20k_vs_ghia1982.png")
+plotme('NeonMultiResLBM_5000.dat', 'Neon MultiRes LBM', 'MultiResNeon_vs_ghia1982')
+plotme('NeonUniformLBM_20000.dat', 'Neon Uniform LBM',  'UniformNeon_vs_ghia1982.png')
