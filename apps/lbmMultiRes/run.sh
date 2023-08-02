75c248440f3cda7e32286379e13d917f9ebd1bab
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-08-02 15:46:53 -0400

    update run script

--- apps/lbmMultiRes/run.sh
+++ apps/lbmMultiRes/run.sh
@@ -3,11 +3,27 @@ exe="../../build/bin/app-lbmMultiRes"
 numIter=50
 deviceId=7
 
-for problemId in 1 2 3 4 5 6 7 8 9 10; do
+for problemId in 1 2 3 4 5 6 7 8 9; do
 for dataType in "float" "double"; do
-echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType
-${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType
-echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --collisionFusedStore --streamFusedExpl --dataType $dataType
-${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --collisionFusedStore --streamFusedExpl --dataType $dataType
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType --storeCoarse
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType --storeCoarse
+
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --storeFine
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --storeFine
+
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --storeFine  --collisionFusedStore
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --storeFine  --collisionFusedStore
+
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore
+
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore  --streamFusedExpl 
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore  --streamFusedExpl 
+
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore  --streamFusedCoal
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore  --streamFusedCoal
+
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore  --streamFuseAll
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemId $problemId --benchmark --dataType $dataType  --collisionFusedStore  --streamFuseAll
 done 
-done 
+done
