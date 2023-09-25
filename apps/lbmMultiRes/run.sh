cd467ffebca4ae9e71535a8c438d38a752da2042
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-09-25 17:59:14 -0400

    Update run.sh

--- apps/lbmMultiRes/run.sh
+++ apps/lbmMultiRes/run.sh
@@ -7,8 +7,8 @@ for scale in 1 2 3 4 5 6 7 8 9; do
 for dataType in "float" "double"; do
 for collideOption in "--storeCoarse" "--storeFine" "--collisionFusedStore" "--fusedFinest --collisionFusedStore"; do
 for streamOption in " " "--streamFusedExpl" "--streamFusedCoal" "--streamFuseAll"; do
-echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemType lid $problemId --benchmark --dataType $dataType --storeCoarse --re 100 --scale $scale $collideOption $streamOption
-${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemType lid $problemId --benchmark --dataType $dataType --storeCoarse --re 100 --scale $scale $collideOption $streamOption
+echo ${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemType lid $problemId --benchmark --dataType $dataType --re 100 --scale $scale $collideOption $streamOption
+${exe} --numIter $numIter --deviceType gpu --deviceId $deviceId --problemType lid $problemId --benchmark --dataType $dataType --re 100 --scale $scale $collideOption $streamOption
 done
 done
 done 
