0ddaee8ca69353b3486b673bb85e658c152f1892
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-10-05 12:59:21 -0400

    ghia plots

--- apps/lbmMultiRes/scripts/plot.py
+++ apps/lbmMultiRes/scripts/plot.py
@@ -7,17 +7,26 @@ def plotme(dat_filename_X="", dat_filename_Y="", label="", png_filename="", scal
     fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 3), dpi=200)
 
     if (dat_filename_Y!=""):
+      
+
         y_neon, u_neon = np.loadtxt(dat_filename_Y, unpack=True, usecols=(0, 1))
+        y_neon -=0.5
         axes.plot(y_neon, scale*u_neon, 'y.-', label=label)
 
+
         y_ref, u_ref = np.loadtxt('ghia1982.dat', unpack=True, skiprows=2, usecols=(0, 1))
+        y_ref -=0.5
         axes.plot(y_ref, u_ref, 'bs', markerfacecolor='none', label='Ghia et al. 1982')
+
+        plt.xticks([-0.5, -0.25, 0.0,  0.25, 0.5])
 
     if (dat_filename_X!=""):        
         x_neon, v_neon = np.loadtxt(dat_filename_X, unpack=True, usecols=(0, 1))
+        x_neon -= 0.5
         axes.plot(x_neon, scale*v_neon, 'g.-', label=label)    
 
         x_ref, v_ref = np.loadtxt('ghia1982.dat', unpack=True, skiprows=2, usecols=(6, 7))
+        x_ref -= 0.5
         axes.plot(x_ref, v_ref, 'ks', markerfacecolor='none',
                   label='Ghia et al. 1982')
 
