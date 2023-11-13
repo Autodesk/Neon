375c2d10b93cf615b05ce3c52b17619a7c30f21a
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2023-11-13 08:21:31 +0100

    WIP

--- libNeonDomain/include/Neon/domain/details/bGridDisg/ClassificationGrid/cIndex.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Neon/domain/details/bGridDisg/bIndex.h"
-
-namespace Neon::domain::details::disaggregated::bGrid::cGrid {
-    template<Block>
-    cIndex = Neon::domain::details::disaggregated::bGrid::bIndex<Block>;
-}
