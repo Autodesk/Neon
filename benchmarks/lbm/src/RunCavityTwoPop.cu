48acb7f47c671de03e723bb15b30d8d7f6ffe869
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2023-12-20 18:08:43 +0100

    Re-enable KBC in the CLI

--- benchmarks/lbm/src/RunCavityTwoPop.cu
+++ benchmarks/lbm/src/RunCavityTwoPop.cu
@@ -135,18 +135,18 @@ auto runFilterCollision(Config&            config,
         testCode << "_bgk";
         return runFilterMethod<Collision::bgk, Lattice, Grid, Storage, Compute>(config, report, testCode);
     }
-    //    if (config.collisionCli.getOption() == Collision::kbc) {
-    //        if (config.lattice != "d3q27" && config.lattice != "D3Q27") {
-    //            Neon::NeonException e("runFilterCollision");
-    //            e << "LBM kbc collision model only supports d3q27 lattice";
-    //            NEON_THROW(e);
-    //        }
-    //        testCode << "_kbc";
-    //        using L = D3Q27<Precision<Storage, Compute>>;
-    //        if constexpr (std::is_same_v<Lattice, L>) {
-    //            return runFilterMethod<Collision::kbc, Lattice, Grid, Storage, Compute>(config, report, testCode);
-    //        }
-    //    }
+    if (config.collisionCli.getOption() == Collision::kbc) {
+        if (config.lattice != "d3q27" && config.lattice != "D3Q27") {
+            Neon::NeonException e("runFilterCollision");
+            e << "LBM kbc collision model only supports d3q27 lattice";
+            NEON_THROW(e);
+        }
+        testCode << "_kbc";
+        using L = D3Q27<Precision<Storage, Compute>>;
+        if constexpr (std::is_same_v<Lattice, L>) {
+            return runFilterMethod<Collision::kbc, Lattice, Grid, Storage, Compute>(config, report, testCode);
+        }
+    }
     NEON_DEV_UNDER_CONSTRUCTION("");
 }
 
