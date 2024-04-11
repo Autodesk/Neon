a704a4d30a72c956578cc4388a845ea2b468005a
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2024-04-11 17:56:13 +0200

    Removing vec2d and vec4d from compilation path of NVRTC.

--- libNeonCore/include/Neon/core/types/vec.h
+++ libNeonCore/include/Neon/core/types/vec.h
@@ -8,25 +8,42 @@
 #pragma warning(disable : 4201)
 #endif
 
+#if !defined(NEON_WARP_COMPILATION)
 #include "Neon/core/types/vec/vec2d_generic.h"
+#endif
+
 #include "Neon/core/types/vec/vec3d_generic.h"
+
+#if !defined(NEON_WARP_COMPILATION)
 #include "Neon/core/types/vec/vec4d_generic.h"
+#endif
+
 
 #include "Neon/core/types/vec/vecAlias.h"
 
+#if !defined(NEON_WARP_COMPILATION)
 #include "Neon/core/types/vec/vec2d_integer.tdecl.h"
 #include "Neon/core/types/vec/vec2d_real.tdecl.h"
+#endif
+
 #include "Neon/core/types/vec/vec3d_integer.tdecl.h"
 #include "Neon/core/types/vec/vec3d_real.tdecl.h"
+#if !defined(NEON_WARP_COMPILATION)
 #include "Neon/core/types/vec/vec4d_integer.tdecl.h"
 #include "Neon/core/types/vec/vec4d_real.tdecl.h"
+#endif
 
+
+#if !defined(NEON_WARP_COMPILATION)
 #include "Neon/core/types/vec/vec2d_integer.timp.h"
 #include "Neon/core/types/vec/vec2d_real.timp.h"
+#endif
 #include "Neon/core/types/vec/vec3d_integer.timp.h"
 #include "Neon/core/types/vec/vec3d_real.timp.h"
+#if !defined(NEON_WARP_COMPILATION)
 #include "Neon/core/types/vec/vec4d_integer.timp.h"
 #include "Neon/core/types/vec/vec4d_real.timp.h"
+#endif
 
 #if defined(NEON_COMPILER_VS)
 #pragma warning(pop)
