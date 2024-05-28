b7dcaa09923158a8c8152d2a7b8389b732633b19
MATTHEW PECHEN-BERG <matthew.pechen-berg@autodesk.com>
2024-05-28 06:52:17 -0700

    made requested changes to first part of project

--- libNeonPy/include/Neon/py/backend.h
+++ libNeonPy/include/Neon/py/backend.h
@@ -6,54 +6,31 @@
 /**
  * Empty constructor
  */
-extern "C" auto dBackend_new(
+extern "C" auto dBackend_new1(
     uint64_t& handle)
     -> int;
 
 /**
  * Creating a Backend object with the first nGpus devices.
  */
-extern "C" auto dBackend_new(
+extern "C" auto dBackend_new2(
     uint64_t& handle,
     int nGpus /*!   Number of devices. The devices are selected in the order specifies by CUDA */,
-    Neon::Runtime runtime /*! Type of runtime to use */)
+    int runtime /*! Type of runtime to use */)
     -> int;
 
 /**
  *
  */
-extern "C" auto dBackend_new(
+extern "C" auto dBackend_new3(
     uint64_t& handle,
-    const std::vector<int>& devIds /*!  Vectors of device ids. There are CUDA device ids */,
-    Neon::Runtime runtime /*! Type of runtime to use */)
+    const int* devIds /*!  Vectors of device ids. There are CUDA device ids */,
+    int runtime /*! Type of runtime to use */)
     -> int;
 
-/**
- *
- */
-extern "C" auto dBackend_new(
-    uint64_t& handle,
-    const Neon::set::DevSet& devSet,
-    Neon::Runtime runtime /*! Type of runtime to use */)
-    -> int;
 
-/**
- *
- */
-extern "C" auto dBackend_new(
-    uint64_t& handle,
-    const std::vector<int>&     devIds,
-    const Neon::set::StreamSet& streamSet)
-    -> int;
 
-/**
- *
- */
-extern "C" auto dBackend_new(
-    uint64_t& handle,
-    const Neon::set::DevSet&    devSet,
-    const Neon::set::StreamSet& streamSet)
-    -> int;
+
 
 /**
  * Delete a backend object on the heap.
@@ -61,3 +38,7 @@ extern "C" auto dBackend_new(
 extern "C" auto dBackend_delete(
     uint64_t& handle)
     -> int;
+
+
+extern "C" auto dBackend_get_string(uint64_t& handle) -> const char*;
+
