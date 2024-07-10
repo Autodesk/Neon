fff0c9b7421b844976fcca0eae7a35b51d4497ca
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2024-07-10 10:23:41 +0200

    WIP

--- libNeonDomain/include/Neon/domain/details/dGrid/dSpan.h
+++ libNeonDomain/include/Neon/domain/details/dGrid/dSpan.h
@@ -4,59 +4,66 @@
 #include "Neon/set/ExecutionThreadSpan.h"
 #include <vector>
 
-namespace Neon::domain::details::dGrid {
-
-/**
- * Abstraction that represents the Cell space of a partition
- * This abstraction is used by the neon lambda executor to
- * run a containers on aGrid
- */
-class dSpan
+namespace Neon::domain::details::dGrid
 {
-   public:
-    using Idx = dIndex;
-    friend class dGrid;
+    /**
+     * Abstraction that represents the Cell space of a partition
+     * This abstraction is used by the neon lambda executor to
+     * run a containers on aGrid
+     */
+    class dSpan
+    {
+    public:
+        using Idx = dIndex;
+        friend class dGrid;
 
-    static constexpr Neon::set::details::ExecutionThreadSpan executionThreadSpan = Neon::set::details::ExecutionThreadSpan::d3;
-    using ExecutionThreadSpanIndexType = int32_t;
+        static constexpr Neon::set::details::ExecutionThreadSpan executionThreadSpan =
+            Neon::set::details::ExecutionThreadSpan::d3;
+        using ExecutionThreadSpanIndexType = int32_t;
 
 
-    NEON_CUDA_HOST_DEVICE inline auto
-    setAndValidate(Idx&            idx,
-                   const uint32_t& x,
-                   const uint32_t& y,
-                   const uint32_t& z) const
-        -> bool;
+        NEON_CUDA_HOST_DEVICE inline auto
+        setAndValidate(Idx& idx,
+                       const uint32_t& x,
+                       const uint32_t& y,
+                       const uint32_t& z) const
+            -> bool;
 
-    NEON_CUDA_HOST_DEVICE inline auto
-    helpGetDataView()
+        NEON_CUDA_HOST_DEVICE inline auto
+        setAndValidate_warp(Idx& idx,
+                            const uint32_t& x,
+                            const uint32_t& y,
+                            const uint32_t& z) const
+            -> void;
+
+        NEON_CUDA_HOST_DEVICE inline auto
+        helpGetDataView()
         const -> Neon::DataView const&;
 
-    NEON_CUDA_HOST_DEVICE inline auto
-    helpGetZHaloRadius()
+        NEON_CUDA_HOST_DEVICE inline auto
+        helpGetZHaloRadius()
         const -> int const&;
 
-    NEON_CUDA_HOST_DEVICE inline auto
-    helpGetZBoundaryRadius()
+        NEON_CUDA_HOST_DEVICE inline auto
+        helpGetZBoundaryRadius()
         const -> int const&;
 
-    NEON_CUDA_HOST_DEVICE inline auto
-    helpGetDim()
+        NEON_CUDA_HOST_DEVICE inline auto
+        helpGetDim()
         const -> Neon::index_3d const&;
 
-    #if !defined(NEON_WARP_COMPILATION)
-    // Function to get offsets of member variables
-    static void getOffsets(size_t* offsets, size_t* length);
-    #endif
+#if !defined(NEON_WARP_COMPILATION)
+        // Function to get offsets of member variables
+        static void getOffsets(size_t* offsets, size_t* length);
+#endif
 
-   private:
-    Neon::DataView mDataView;
-    int            mZghostRadius;
-    int            mZboundaryRadius;
-    int            mMaxZInDomain;
-    Neon::index_3d mSpanDim /** Dimension of the span, its values depends on the mDataView*/;
-};
-
-}  // namespace Neon::domain::deta  ils::dGrid
+    private:
+        Neon::DataView mDataView;
+        int mZghostRadius;
+        int mZboundaryRadius;
+        int mMaxZInDomain;
+        Neon::index_3d mSpanDim /** Dimension of the span, its values depends on the mDataView*/;
+    };
+} // namespace Neon::domain::deta  ils::dGrid
 
 #include "dSpan_imp.h"
--- libNeonDomain/include/Neon/domain/details/dGrid/dSpan_imp.h
+++ libNeonDomain/include/Neon/domain/details/dGrid/dSpan_imp.h
@@ -1,93 +1,149 @@
 #pragma once
 
-namespace Neon::domain::details::dGrid {
-
-NEON_CUDA_HOST_DEVICE inline auto
-dSpan::setAndValidate(Idx&            idx,
-                      const uint32_t& x,
-                      const uint32_t& y,
-                      const uint32_t& z)
-    const -> bool
+namespace Neon::domain::details::dGrid
 {
-    bool res = false;
+    NEON_CUDA_HOST_DEVICE inline auto
+    dSpan::setAndValidate(Idx& idx,
+                          const uint32_t& x,
+                          const uint32_t& y,
+                          const uint32_t& z)
+    const -> bool
+    {
+        bool res = false;
 
 #if !defined(NEON_WARP_COMPILATION)
-    idx.setLocation().x = int(x);
-    idx.setLocation().y = int(y);
-    idx.setLocation().z = int(z);
+        idx.setLocation().x = int(x);
+        idx.setLocation().y = int(y);
+        idx.setLocation().z = int(z);
 #else
    idx.setLocation().x = x;
     idx.setLocation().y = y;
     idx.setLocation().z = z;
- #endif
-    if (idx.getLocation() < mSpanDim) {
-        res = true;
+#endif
+        if (idx.getLocation() < mSpanDim)
+        {
+            res = true;
+        }
+
+        switch (mDataView)
+        {
+        case Neon::DataView::STANDARD:
+            {
+                idx.setLocation().z += mZghostRadius;
+                return res;
+            }
+        case Neon::DataView::INTERNAL:
+            {
+                idx.setLocation().z += mZghostRadius + mZboundaryRadius;
+                return res;
+            }
+        case Neon::DataView::BOUNDARY:
+            {
+                idx.setLocation().z += idx.getLocation().z < mZboundaryRadius
+                                           ? 0
+                                           : (mMaxZInDomain - 1) + (-1 * mZboundaryRadius
+                                               /* we remove zBoundaryRadius as the first zBoundaryRadius will manage the lower slices */);
+                idx.setLocation().z += mZghostRadius;
+
+                return res;
+            }
+        default:
+            {
+            }
+        }
+        return false;
     }
 
-    switch (mDataView) {
-        case Neon::DataView::STANDARD: {
-            idx.setLocation().z += mZghostRadius;
-            return res;
-        }
-        case Neon::DataView::INTERNAL: {
-            idx.setLocation().z += mZghostRadius + mZboundaryRadius;
-            return res;
-        }
-        case Neon::DataView::BOUNDARY: {
-
-            idx.setLocation().z += idx.getLocation().z < mZboundaryRadius
-                                       ? 0
-                               : (mMaxZInDomain - 1) + (-1 * mZboundaryRadius /* we remove zBoundaryRadius as the first zBoundaryRadius will manage the lower slices */);
-            idx.setLocation().z += mZghostRadius;
-
-            return res;
-        }
-        default: {
-        }
-    }
-    return false;
-}
-
-NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetDataView()
-    const -> Neon::DataView const&
-{
-    return mDataView;
-}
-
-NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetZHaloRadius()
-    const -> int const&
-{
-    return mZghostRadius;
-}
-
-NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetZBoundaryRadius()
-    const -> int const&
-{
-    return mZboundaryRadius;
-}
-
-NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetDim()
-    const -> Neon::index_3d const&
-{
-    return mSpanDim;
-}
+    NEON_CUDA_HOST_DEVICE inline auto
+    dSpan::setAndValidate_warp(Idx& idx,
+                               const uint32_t& x,
+                               const uint32_t& y,
+                               const uint32_t& z)
+    const -> void
+    {
+        printf("setAndValidate_warp: %d %d %d\n" ,x , y , z );
 
 #if !defined(NEON_WARP_COMPILATION)
-inline void dSpan::getOffsets(size_t* offsets, size_t* length) {
-    std::cout << "dGrid_dSpan cpp offsets: " << offsetof(dSpan, mDataView) << " " << offsetof(dSpan, mZghostRadius) << " " << offsetof(dSpan, mZboundaryRadius) << " " << offsetof(dSpan, mMaxZInDomain) << " " << offsetof(dSpan, mSpanDim) << " " <<  std::endl;
-    static std::vector<size_t> cpp_offsets = {
-        offsetof(dSpan, mDataView),
-        offsetof(dSpan, mZghostRadius),
-        offsetof(dSpan, mZboundaryRadius),
-        offsetof(dSpan, mMaxZInDomain),
-        offsetof(dSpan, mSpanDim)
-    };
-    
-    *length = cpp_offsets.size();
-    for (size_t i = 0; i < cpp_offsets.size(); ++i) {
-        offsets[i] = cpp_offsets[i];
-    }
-}
+        idx.setLocation().x = int(x);
+        idx.setLocation().y = int(y);
+        idx.setLocation().z = int(z);
+#else
+    idx.setLocation().x = x;
+    idx.setLocation().y = y;
+    idx.setLocation().z = z;
 #endif
 
-}  // namespace Neon::domain::details::dGrid
+        switch (mDataView)
+        {
+        case Neon::DataView::STANDARD:
+            {
+                idx.setLocation().z += mZghostRadius;
+                return;
+            }
+        case Neon::DataView::INTERNAL:
+            {
+                idx.setLocation().z += mZghostRadius + mZboundaryRadius;
+                return;
+            }
+        case Neon::DataView::BOUNDARY:
+            {
+                idx.setLocation().z += idx.getLocation().z < mZboundaryRadius
+                                           ? 0
+                                           : (mMaxZInDomain - 1) + (-1 * mZboundaryRadius
+                                               /* we remove zBoundaryRadius as the first zBoundaryRadius will manage the lower slices */);
+                idx.setLocation().z += mZghostRadius;
+
+                return;
+            }
+        default:
+            {
+            }
+        }
+    }
+
+    NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetDataView()
+    const -> Neon::DataView const&
+    {
+        return mDataView;
+    }
+
+    NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetZHaloRadius()
+    const -> int const&
+    {
+        return mZghostRadius;
+    }
+
+    NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetZBoundaryRadius()
+    const -> int const&
+    {
+        return mZboundaryRadius;
+    }
+
+    NEON_CUDA_HOST_DEVICE inline auto dSpan::helpGetDim()
+    const -> Neon::index_3d const&
+    {
+        return mSpanDim;
+    }
+
+#if !defined(NEON_WARP_COMPILATION)
+    inline void dSpan::getOffsets(size_t* offsets, size_t* length)
+    {
+        std::cout << "dGrid_dSpan cpp offsets: " << offsetof(dSpan, mDataView) << " " << offsetof(dSpan, mZghostRadius)
+            << " " << offsetof(dSpan, mZboundaryRadius) << " " << offsetof(dSpan, mMaxZInDomain) << " " <<
+            offsetof(dSpan, mSpanDim) << " " << std::endl;
+        static std::vector<size_t> cpp_offsets = {
+            offsetof(dSpan, mDataView),
+            offsetof(dSpan, mZghostRadius),
+            offsetof(dSpan, mZboundaryRadius),
+            offsetof(dSpan, mMaxZInDomain),
+            offsetof(dSpan, mSpanDim)
+        };
+
+        *length = cpp_offsets.size();
+        for (size_t i = 0; i < cpp_offsets.size(); ++i)
+        {
+            offsets[i] = cpp_offsets[i];
+        }
+    }
+#endif
+} // namespace Neon::domain::details::dGrid
